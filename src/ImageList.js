2f49032db7e535389db5114dabc1e6b7cbfa034e
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-01-31 21:44:23 -0500

    Passing url and caption to list

--- src/ImageList.js
+++ src/ImageList.js
@@ -1,16 +1,18 @@
 import "./App.css";
-function ImageList() {
-  // add to this list?
+function ImageList({ images }) {
+  // This function is passed an array of images.
   return (
     <ul>
       <li>
         <img src="https://images.unsplash.com/photo-1591096490931-c07a285f8068?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"></img>
         <figcaption>A cab crosses the road in the Lower East Side</figcaption>
       </li>
-      <li>
-        {/*<img src={initialImages.imageURL}></img>
-        <figcaption>{initialImages.caption}</figcaption> */}
-      </li>
+      {images.map((image, index) => (
+        <li key={index}>
+          <img src={image.imageURL}></img>
+          <figcaption>{image.caption}</figcaption>
+        </li>
+      ))}
     </ul>
   );
 }
